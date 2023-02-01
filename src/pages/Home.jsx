2cae2f6335864dd729863b176294a50f2b901091
Eric Venturino <eric.venturino@orange.fr>
2023-02-01 07:14:09 +0100

    Envoi 1er fevrier

---
+++ src/pages/Home.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Card from "../components/Card";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import "../styles/home.scss";
+
+// Choix de la page d'accueil au hasard
+const pages = ["Fruits", "Aromates", "Legumes"];
+
+// Item nombre aléatoire entre 1 et 3
+const Item = pages[Math.floor(Math.random() * (2 - 0 + 1)) + 0];
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+      <div className="hero-div">
+        <img
+          src={require(`../images/${Item}.jpg`)}
+          alt="Fond"
+          className="hero"
+        />
+        <div className="titre-site">
+        <h1 >
+          Le potager "Le bonheur de manger son chez soi".
+        </h1>
+        </div>
+                </div>
+        
+
+        <section>
+          <h2>Venez partager un moment de détente et de nature</h2>
+          <div className="boutonduo">
+            <a href={`/${Item}`}>
+              {" "}
+              <button className="btn1"> {Item}</button>
+            </a>
+            <a href={`/Journal`}>
+            <button className="btn2">Voir le journal</button>
+            </a>
+          </div>
+
+          <h3>O Potager Perso c'est quoi ?</h3>
+          <p>
+            Pendant cette dure periode ou les couts de produition de vos plats
+            alimentaire industriel explosent. Apprendre a revenir à nos racines
+            et cultiver son propre potager (en jardière sur votre balcon, en
+            terre dans un coin de votre jardin ou dans une mini serre) peut
+            importe la taille pour se nourrir de manière plus écolo, plus saine
+            et plus économique. O Potager Perso c'est avant tout de l'entraide
+            pour la réalisation de votre potager personnel
+          </p>
+        </section>
+
+        <section className="carte">
+          <Card />
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
